4618f5dace3dd17604286a68684d837a6addeb7b
josem927 <2001ohdude@gmail.com>
2023-11-12 23:24:43 -0600

    cambios para que cheques las imagenes

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex-1 p-6">
+      <div class="flex items-center justify-center">
+        <div class="max-w-full max-h-full flex">
+          <img :src="imageSrc" class="max-w-1/2 max-h-full" />
+          <div class="text-left ml-4">
+            <h1 class="font-serif font-bold text-4xl">{{ title }}</h1>
+            <p class="text-left">{{ content }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  const props = defineProps(['imageSrc', 'title', 'content']);
+  </script>
+  
---
+++ resources/js/Pages/__VLS_template.ts
@@ -0,0 +1,125 @@
+import { __VLS_internalComponent, __VLS_componentsOption, __VLS_name } from './Welcome.vue';
+
+function __VLS_template() {
+let __VLS_ctx!: InstanceType<__VLS_PickNotAny<typeof __VLS_internalComponent, new () => {}>> & {};
+/* Components */
+let __VLS_otherComponents!: NonNullable<typeof __VLS_internalComponent extends { components: infer C; } ? C : {}> & typeof __VLS_componentsOption;
+let __VLS_own!: __VLS_SelfComponent<typeof __VLS_name, typeof __VLS_internalComponent & (new () => { $slots: typeof __VLS_slots; })>;
+let __VLS_localComponents!: typeof __VLS_otherComponents & Omit<typeof __VLS_own, keyof typeof __VLS_otherComponents>;
+let __VLS_components!: typeof __VLS_localComponents & __VLS_GlobalComponents & typeof __VLS_ctx;
+/* Style Scoped */
+type __VLS_StyleScopedClasses = {};
+let __VLS_styleScopedClasses!: __VLS_StyleScopedClasses | keyof __VLS_StyleScopedClasses | (keyof __VLS_StyleScopedClasses)[];
+/* CSS variable injection */
+/* CSS variable injection end */
+let __VLS_resolvedLocalAndGlobalComponents!: {} &
+__VLS_WithComponent<'GmapMap', typeof __VLS_localComponents, "GmapMap", "GmapMap", "GmapMap"> &
+__VLS_WithComponent<'GmapMarker', typeof __VLS_localComponents, "GmapMarker", "GmapMarker", "GmapMarker">;
+({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).img;
+({} as __VLS_IntrinsicElements).h1; ({} as __VLS_IntrinsicElements).h1;
+__VLS_components.GmapMap;
+// @ts-ignore
+[GmapMap,];
+__VLS_components.GmapMarker;
+// @ts-ignore
+[GmapMarker,];
+{
+const __VLS_0 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_1 = __VLS_elementAsFunctionalComponent(__VLS_0);
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_2 = __VLS_1({ ...{}, class: ("flex h-screen bg-gray-100 dark:bg-gray-900"), }, ...__VLS_functionalComponentArgsRest(__VLS_1));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_0, typeof __VLS_2> & Record<string, unknown>) => void)({ ...{}, class: ("flex h-screen bg-gray-100 dark:bg-gray-900"), });
+const __VLS_3 = __VLS_pickFunctionalComponentCtx(__VLS_0, __VLS_2)!;
+let __VLS_4!: __VLS_NormalizeEmits<typeof __VLS_3.emit>;
+{
+const __VLS_5 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_6 = __VLS_elementAsFunctionalComponent(__VLS_5);
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_7 = __VLS_6({ ...{}, class: ("w-64 bg-sidebar p-6 rounded-r-lg"), }, ...__VLS_functionalComponentArgsRest(__VLS_6));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_5, typeof __VLS_7> & Record<string, unknown>) => void)({ ...{}, class: ("w-64 bg-sidebar p-6 rounded-r-lg"), });
+const __VLS_8 = __VLS_pickFunctionalComponentCtx(__VLS_5, __VLS_7)!;
+let __VLS_9!: __VLS_NormalizeEmits<typeof __VLS_8.emit>;
+(__VLS_8.slots!).default;
+}
+{
+const __VLS_10 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_11 = __VLS_elementAsFunctionalComponent(__VLS_10);
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_12 = __VLS_11({ ...{}, class: ("flex-1 p-6"), }, ...__VLS_functionalComponentArgsRest(__VLS_11));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_10, typeof __VLS_12> & Record<string, unknown>) => void)({ ...{}, class: ("flex-1 p-6"), });
+const __VLS_13 = __VLS_pickFunctionalComponentCtx(__VLS_10, __VLS_12)!;
+let __VLS_14!: __VLS_NormalizeEmits<typeof __VLS_13.emit>;
+{
+const __VLS_15 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_16 = __VLS_elementAsFunctionalComponent(__VLS_15);
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_17 = __VLS_16({ ...{}, class: ("flex items-center justify-center"), }, ...__VLS_functionalComponentArgsRest(__VLS_16));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_15, typeof __VLS_17> & Record<string, unknown>) => void)({ ...{}, class: ("flex items-center justify-center"), });
+const __VLS_18 = __VLS_pickFunctionalComponentCtx(__VLS_15, __VLS_17)!;
+let __VLS_19!: __VLS_NormalizeEmits<typeof __VLS_18.emit>;
+{
+const __VLS_20 = ({} as __VLS_IntrinsicElements)["img"];
+const __VLS_21 = __VLS_elementAsFunctionalComponent(__VLS_20);
+({} as __VLS_IntrinsicElements).img;
+const __VLS_22 = __VLS_21({ ...{}, src: ("/images/informativo.jpg"), class: ("max-w-full max-h-full"), }, ...__VLS_functionalComponentArgsRest(__VLS_21));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_20, typeof __VLS_22> & Record<string, unknown>) => void)({ ...{}, src: ("/images/informativo.jpg"), class: ("max-w-full max-h-full"), });
+const __VLS_23 = __VLS_pickFunctionalComponentCtx(__VLS_20, __VLS_22)!;
+let __VLS_24!: __VLS_NormalizeEmits<typeof __VLS_23.emit>;
+}
+{
+const __VLS_25 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_26 = __VLS_elementAsFunctionalComponent(__VLS_25);
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_27 = __VLS_26({ ...{}, class: ("text-center mt-4"), }, ...__VLS_functionalComponentArgsRest(__VLS_26));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_25, typeof __VLS_27> & Record<string, unknown>) => void)({ ...{}, class: ("text-center mt-4"), });
+const __VLS_28 = __VLS_pickFunctionalComponentCtx(__VLS_25, __VLS_27)!;
+let __VLS_29!: __VLS_NormalizeEmits<typeof __VLS_28.emit>;
+{
+const __VLS_30 = ({} as __VLS_IntrinsicElements)["h1"];
+const __VLS_31 = __VLS_elementAsFunctionalComponent(__VLS_30);
+({} as __VLS_IntrinsicElements).h1;
+({} as __VLS_IntrinsicElements).h1;
+const __VLS_32 = __VLS_31({ ...{}, class: ("font-serif font-bold text-4xl"), }, ...__VLS_functionalComponentArgsRest(__VLS_31));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_30, typeof __VLS_32> & Record<string, unknown>) => void)({ ...{}, class: ("font-serif font-bold text-4xl"), });
+const __VLS_33 = __VLS_pickFunctionalComponentCtx(__VLS_30, __VLS_32)!;
+let __VLS_34!: __VLS_NormalizeEmits<typeof __VLS_33.emit>;
+(__VLS_33.slots!).default;
+}
+(__VLS_28.slots!).default;
+}
+{
+let __VLS_35!: 'GmapMap' extends keyof typeof __VLS_ctx ? typeof __VLS_ctx.GmapMap : (typeof __VLS_resolvedLocalAndGlobalComponents)['GmapMap'];
+const __VLS_36 = __VLS_asFunctionalComponent(__VLS_35, new __VLS_35({ ...{}, center: (({ lat: 17.5583528, lng: -92.9737064 })), style: ({}), }));
+({} as { GmapMap: typeof __VLS_35; }).GmapMap;
+const __VLS_37 = __VLS_36({ ...{}, center: (({ lat: 17.5583528, lng: -92.9737064 })), style: ({}), }, ...__VLS_functionalComponentArgsRest(__VLS_36));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_35, typeof __VLS_37> & Record<string, unknown>) => void)({ ...{}, center: (({ lat: 17.5583528, lng: -92.9737064 })), style: ({}), });
+const __VLS_38 = __VLS_pickFunctionalComponentCtx(__VLS_35, __VLS_37)!;
+let __VLS_39!: __VLS_NormalizeEmits<typeof __VLS_38.emit>;
+{
+let __VLS_40!: 'GmapMarker' extends keyof typeof __VLS_ctx ? typeof __VLS_ctx.GmapMarker : (typeof __VLS_resolvedLocalAndGlobalComponents)['GmapMarker'];
+const __VLS_41 = __VLS_asFunctionalComponent(__VLS_40, new __VLS_40({ ...{}, position: (({ lat: 17.5583528, lng: -92.9737064 })), title: ("Ubicación Exacta"), "<": (true), div: (true), }));
+({} as { GmapMarker: typeof __VLS_40; }).GmapMarker;
+const __VLS_42 = __VLS_41({ ...{}, position: (({ lat: 17.5583528, lng: -92.9737064 })), title: ("Ubicación Exacta"), "<": (true), div: (true), }, ...__VLS_functionalComponentArgsRest(__VLS_41));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_40, typeof __VLS_42> & Record<string, unknown>) => void)({ ...{}, position: (({ lat: 17.5583528, lng: -92.9737064 })), title: ("Ubicación Exacta"), "<": (true), div: (true), });
+const __VLS_43 = __VLS_pickFunctionalComponentCtx(__VLS_40, __VLS_42)!;
+let __VLS_44!: __VLS_NormalizeEmits<typeof __VLS_43.emit>;
+}
+(__VLS_38.slots!).default;
+}
+(__VLS_18.slots!).default;
+}
+(__VLS_13.slots!).default;
+}
+(__VLS_3.slots!).default;
+}
+if (typeof __VLS_styleScopedClasses === 'object' && !Array.isArray(__VLS_styleScopedClasses)) {
+}
+var __VLS_slots!: {};
+return __VLS_slots;
+}
