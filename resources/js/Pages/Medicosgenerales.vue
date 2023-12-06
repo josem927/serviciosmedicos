ba458b903f65b3e8b85e4215fbb20d4a048c24ac
Kayler453 <faustocalethp@gmail.com>
2023-12-06 01:42:34 -0600

    Card

--- resources/js/Pages/Medicosgenerales.vue
+++ resources/js/Pages/Medicosgenerales.vue
@@ -1,29 +1,33 @@
-  <template>
+<template>
+  <div>
     <Head title="Medico General" />
-   <div class="background-image">
-    <img src="/images/general.jpg" class="max-w-full h-full object-cover" />
-  </div>
+    <div class="background-image">
+      <img src="/images/general.jpg" class="max-w-full h-full object-cover" />
+    </div>
     <div class="flex flex-col items-center">
       <div class="container">
-        <div v-if="medicoData.length > 0" class="mt-8 flex flex-wrap -mx-4">
-          <div v-for="medico in medicogral" :key="medico.id" class="card mx-4 mb-4">
+        <div v-if="medicoData.length > 0" class="mt-8 flex flex-wrap -mx-2">
+          <div v-for="medico in medicogral" :key="medico.id" 
+            class="card mx-4 mb-4" 
+            :class="'card-' + medico.id"
+            @mouseover="changeTextColor(medico.id, true)" 
+            @mouseout="changeTextColor(medico.id, false)">
             <div class="md:flex h-full">
-              <div class="md:flex-shrink-0">
-              </div>
+              <div class="md:flex-shrink-0"></div>
               <div class="p-8 flex flex-col justify-between items-center w-full h-full">
-                <div class="flex flex-col mt-60">
-                  <div class="uppercase tracking-wide text-sm text-black font-semibold">
-                    <h1>Tipo de médico: {{ medico.userType }}</h1>
+                <div class="flex flex-col mt-48">
+                  <div class="uppercase tracking-wide text-sm text-black font-semibold text-center">
+                    <h1>Tipo: {{ medico.userType }}</h1>
                   </div>
                   
-                  <h2 class="block mt-2 text-lg leading-tight font-medium text-black">
+                  <h2 class="block mt-4 text-lg leading-tight font-medium text-white">
                     <h1>Nombre del consultorio: {{ medico.name }}</h1>
                   </h2>
-                  <p class="mt-2 text-black"><h1>Correo: </h1>{{ medico.email }}</p>
-                  <p class="mt-2 text-black"><h1>Cédula Profesional: </h1>{{ medico.professional_id }}</p>
-                  <p class="mt-2 text-black"><h1>Nombre del doctor: </h1>{{ medico.name_doctor }}</p>
-                  <p class="mt-2 text-black"><h1>Teléfono: </h1><a :href="'whatsapp://' + medico.phone">{{ medico.phone }}</a></p>
-                  <p class="mt-2 text-black"><h1>Ubicación: </h1>{{ medico.ubicacion }}</p>
+                  <p class="mt-2 text-white"><h1>Correo: </h1>{{ medico.email }}</p>
+                  <p class="mt-2 text-white"><h1>Cédula Profesional: </h1>{{ medico.professional_id }}</p>
+                  <p class="mt-2 text-white"><h1>Nombre del doctor: </h1>{{ medico.name_doctor }}</p>
+                  <p class="mt-2 text-white"><h1>Teléfono: </h1><a :href="'whatsapp://' + medico.phone">{{ medico.phone }}</a></p>
+                  <p class="mt-2 text-white"><h1>Ubicación: </h1>{{ medico.ubicacion }}</p>
                 </div>
               </div>
             </div>
@@ -33,7 +37,8 @@
         <p v-else>No hay datos de médicos generales.</p>
       </div>
     </div>
-  </template>
+  </div>
+</template>
 
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
@@ -46,12 +51,35 @@ if (!props.medicogral || props.medicogral.length === 0) {
 }
 
 const medicoData = props.medicogral;
+
+const changeTextColor = (medicoId, isHover) => {
+  const cardTexts = document.querySelectorAll(`.card-${medicoId} .text-white, .card-${medicoId} .text-black`);
+  const textColor = isHover ? 'black' : 'white';
+  cardTexts.forEach((text) => {
+    text.style.color = textColor;
+  });
+
+  // Almacenar el estado en localStorage
+  localStorage.setItem(`card-${medicoId}`, textColor);
+};
+const restoreTextColors = () => {
+  for (let i = 0; i < medicoData.length; i++) {
+    const medicoId = medicoData[i].id;
+    const storedColor = localStorage.getItem(`card-${medicoId}`);
+    if (storedColor) {
+      const cardTexts = document.querySelectorAll(`.card-${medicoId} .text-black, .card-${medicoId} .text-white`);
+      cardTexts.forEach((text) => {
+        text.style.color = storedColor;
+      });
+
+// Restaurar los colores al cargar la página
+restoreTextColors();
+    }
+  }
+};
 </script>
 
 <style>
-.text-super-black {
-  color: #000; 
-}
 
 .background-image {
   position: fixed;
@@ -72,34 +100,37 @@ const medicoData = props.medicogral;
 .card {
   flex: 0 0 calc(33.333% - 20px);
   width: 15cm;
-  height: 15cm;
+  height: 17.5cm;
   margin: 10px;
-  background: rgba(128, 128, 128, 0.5);
+  background: rgba(39, 37, 37, 0.486); /* Color de fondo */
   border-radius: 20px;
   overflow: hidden;
   box-sizing: border-box;
   position: relative;
-  transition: transform 0.3s, box-shadow 0s;
+  transition: transform 0.3s, box-shadow 0.3s;
 }
 
 .card:hover {
   transform: scale(1.05);
-  box-shadow: 0 0 20px rgba(255, 255, 255, 0.5), 0 0 40px rgba(255, 255, 255, 0.3);
+  box-shadow: 10px 10px 20px #c2c2c2, -10px -10px 20px #ffffff; /* Sombras para el efecto neumórfico */
+}
+.card:hover .text-black {
+  color: black; /* Color del texto cuando se pasa el cursor */
 }
 
-.card:not(:last-child) {
-  margin-right: 10px;
+.card:hover .text-white {
+  color: white; /* Color del texto cuando se pasa el cursor */
 }
 
 .card::before {
   content: '';
   position: absolute;
-  top: -2px;
-  left: -2px;
-  right: -2px;
-  bottom: -2px;
-  border-radius: 22px;
-  background: linear-gradient(45deg);
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-radius: 20px;
+  background: linear-gradient(45deg, #20b2d6, #ffffff); /* Degradado de fondo */
   z-index: -1;
   opacity: 0;
   transition: opacity 0.3s;
@@ -107,5 +138,9 @@ const medicoData = props.medicogral;
 
 .card:hover::before {
   opacity: 1;
+}
+
+.card:not(:last-child) {
+  margin-right: 10px;
 }
 </style>
--- resources/js/Pages/Quiropracticos.vue
+++ resources/js/Pages/Quiropracticos.vue
@@ -5,125 +5,146 @@
       <img src="/images/quiropraxia.jpg" class="max-w-full h-full object-cover" />
     </div>
     <div class="flex flex-col items-center">
-      <div v-if="quiropracticosData.length > 0" class="mt-8">
-        <div v-for="quiropractico in quiropracticosData" :key="quiropractico.id" class="card">
-          <div class="md:flex h-full">
-            <div class="md:flex-shrink-0">
-              <!-- Add content here if needed -->
-            </div>
-            <div class="p-8 flex flex-col justify-between w-full h-full">
-              <div class="flex flex-col mt-32">
-                <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-                  <h1>Tipo de quiropráctico</h1>{{ quiropractico.userType }}
+      <div class="container">
+        <div v-if="quiropracticosData.length > 0" class="mt-8 flex flex-wrap -mx-2">
+          <div v-for="quiropractico in quiropracticosData" :key="quiropractico.id" 
+            class="card mx-4 mb-4" 
+            :class="'card-' + quiropractico.id"
+            @mouseover="changeTextColor(quiropractico.id, true)" 
+            @mouseout="changeTextColor(quiropractico.id, false)">
+            <div class="md:flex h-full">
+              <div class="md:flex-shrink-0"></div>
+              <div class="p-8 flex flex-col justify-between w-full h-full">
+                <div class="flex flex-col mt-32">
+                  <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                    <h1>Tipo de quiropráctico</h1>{{ quiropractico.userType }}
+                  </div>
+                  <h2 class="block mt-2 text-lg leading-tight font-medium text-white">
+                    <h1>Nombre del consultorio: {{ quiropractico.name }}</h1>
+                  </h2>
+                  <p class="mt-2 text-black"><h1>Correo: </h1>{{ quiropractico.email }}</p>
+                  <p class="mt-2 text-black"><h1>Cédula Profesional: </h1>{{ quiropractico.professional_id }}</p>
+                  <p class="mt-2 text-gray-500 text-white"><h1>Nombre del doctor: </h1>{{ quiropractico.name_doctor }}</p>
+                  <p class="mt-2 text-gray-500 text-white"><h1>Teléfono: </h1><a :href="'whatsapp://' + quiropractico.phone">{{ quiropractico.phone }}</a></p>
+                  <p class="mt-2 text-gray-500 text-white"><h1>Ubicación: </h1>{{ quiropractico.ubicacion }}</p>
                 </div>
-                <h2 class="block mt-2 text-lg leading-tight font-medium text-white">
-                  <h1>Nombre del consultorio: {{ quiropractico.name }}</h1>
-                </h2>
-                <p class="mt-2 text-black"><h1>Correo: </h1>{{ quiropractico.email }}</p>
-                <p class="mt-2 text-black"><h1>Cédula Profesional: </h1>{{ quiropractico.professional_id }}</p>
-                <p class="mt-2 text-gray-500 text-white"><h1>Nombre del doctor: </h1>{{ quiropractico.name_doctor }}</p>
-                <p class="mt-2 text-gray-500 text-white"><h1>Teléfono: </h1><a :href="'whatsapp://' + quiropractico.phone">{{ quiropractico.phone }}</a></p>
-                <p class="mt-2 text-gray-500 text-white"><h1>Ubicación: </h1>{{ quiropractico.ubicacion }}</p>
               </div>
             </div>
+            <hr class="border-t-2 border-gray-300 my-6">
           </div>
-          <hr class="border-t-2 border-gray-300 my-6">
         </div>
+        <p class="mt-2 text-white" v-else>No hay datos de quiroprácticos.</p>
       </div>
-      <p class="mt-2 text-white" v-else>No hay datos de quiroprácticos.</p>
     </div>
   </div>
-  </template>
-  
-<script setup lang="ts">
+</template>
+
+<script setup>
 import { Head, Link } from '@inertiajs/vue3';
 import { defineProps } from 'vue';
-  
-    const props = defineProps(['Quiropracticos']);
-  
-    if (!props.Quiropracticos || props.Quiropracticos.length === 0) {
-      console.warn('No hay datos de quiroprácticos.');
+
+const props = defineProps(['Quiropracticos']);
+
+if (!props.Quiropracticos || props.Quiropracticos.length === 0) {
+  console.warn('No hay datos de quiroprácticos.');
+}
+
+const quiropracticosData = props.Quiropracticos;
+
+const changeTextColor = (quiropracticoId, isHover) => {
+  const cardTexts = document.querySelectorAll(`.card-${quiropracticoId} .text-white, .card-${quiropracticoId} .text-black`);
+  const textColor = isHover ? 'black' : 'white';
+  cardTexts.forEach((text) => {
+    text.style.color = textColor;
+  });
+
+  // Almacenar el estado en localStorage
+  localStorage.setItem(`card-${quiropracticoId}`, textColor);
+};
+
+const restoreTextColors = () => {
+  for (let i = 0; i < quiropracticosData.length; i++) {
+    const quiropracticoId = quiropracticosData[i].id;
+    const storedColor = localStorage.getItem(`card-${quiropracticoId}`);
+    if (storedColor) {
+      const cardTexts = document.querySelectorAll(`.card-${quiropracticoId} .text-black, .card-${quiropracticoId} .text-white`);
+      cardTexts.forEach((text) => {
+        text.style.color = storedColor;
+      });
     }
-  
-    const quiropracticosData = props.Quiropracticos;
-  </script>
-  
-  <style>
-  .background-image {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    z-index: -1;
-    overflow: hidden;
   }
-  
-  .background-image img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-  
-  body {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0;
-    height: 100vh;
-  }
-  
-  .container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: flex-start;
-    height: 100%;
-  }
-  
-  .card {
-    flex: 0 0 calc(33.333% - 20px); /* 3 en 3 con márgenes */
-    width: 15cm;
-    height: 15cm;
-    margin: 10px;
-    background: rgba(128, 128, 128, 0.5);
-    border-radius: 20px;
-    overflow: hidden;
-    box-sizing: border-box;
-    position: relative;
-    transition: transform 0.3s, box-shadow 0.3s;
-  }
-  
-  .card:hover {
-    transform: scale(1.05); /* Escala al hacer hover para efecto 3D */
-    box-shadow: 0 0 20px rgba(255, 255, 255, 0.5), 0 0 40px rgba(255, 255, 255, 0.3); /* Sombra al hacer hover */
-  }
-  
-  .card:nth-child(3n) {
-    margin-right: 0;
-  }
-  
-  .container .card:nth-child(3n) {
-    margin-right: 0;
-  }
-  
-  /* Efecto de borde neon */
-  .card::before {
-    content: '';
-    position: absolute;
-    top: -2px;
-    left: -2px;
-    right: -2px;
-    bottom: -2px;
-    border-radius: 22px;
-    background: linear-gradient(45deg);
-    z-index: -1;
-    opacity: 0;
-    transition: opacity 0.3s;
-  }
-  
-  .card:hover::before {
-    opacity: 1;
-  }
-  </style>
-  
+};
+
+// Restaurar los colores al cargar la página
+restoreTextColors();
+</script>
+
+<style>
+.background-image {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+  overflow: hidden;
+}
+
+.background-image img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100%;
+}
+
+.card {
+  flex: 0 0 calc(33.333% - 20px);
+  width: 15cm;
+  height: 17.5cm;
+  margin: 10px;
+  background: rgba(128, 128, 128, 0.5);
+  border-radius: 20px;
+  overflow: hidden;
+  box-sizing: border-box;
+  position: relative;
+  transition: transform 0.3s, box-shadow 0.3s;
+}
+
+.card:hover {
+  transform: scale(1.05);
+  box-shadow: 0 0 20px rgba(255, 255, 255, 0.5), 0 0 40px rgba(255, 255, 255, 0.3);
+}
+
+.card:nth-child(3n) {
+  margin-right: 0;
+}
+
+.container .card:nth-child(3n) {
+  margin-right: 0;
+}
+
+.card::before {
+  content: '';
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  right: -2px;
+  bottom: -2px;
+  border-radius: 22px;
+  background: linear-gradient(45deg);
+  z-index: -1;
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.card:hover::before {
+  opacity: 1;
+}
+</style>
